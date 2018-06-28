921bc7fb523160266b2d6cfb8eff08f40fcba72b
Mikhail <mr.dolgih.2000@mail.ru>
2018-06-28 19:03:52 +0300

    C4/custom/1/optimal_search.pas

---
+++ C4/custom/1/optimal_search.pas
@@ -0,0 +1,30 @@
+program optimal_search;
+
+const
+  Lim = 1000;
+
+var
+  i, N, max, q, x: integer;
+
+begin
+  readln(N);
+
+  readln(x);
+  max := x;
+  q := 1;
+  for i := 2 to N do
+  begin
+    readln(x);
+    if x = max then
+      q := q + 1
+    else if x > max then
+    begin
+      max := x;
+      q := 1;
+    end;
+  end;
+
+  writeln(q);
+  readln();
+end.
+
