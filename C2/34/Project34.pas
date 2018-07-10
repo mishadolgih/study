850788c302788295a36a3d7c8674c0e1bd2f0488
Mikhail <mr.dolgih.2000@mail.ru>
2018-07-10 23:30:53 +0300

    C2

--- C2/34/Project34.pas
+++ C2/34/Project34.pas
@@ -1,11 +1,7 @@
 program Project34;
 
 uses
-<<<<<<< HEAD
   Math;
-=======
-  math;
->>>>>>> 6ce2934fa99388e1d03ccbb46210fec37bb88044
 
 const
   N = 70;
@@ -14,24 +10,13 @@ var
   A: array[1..N] of integer;
   i, j, x, y: integer;
 
-<<<<<<< HEAD
 begin
-=======
-begin;
->>>>>>> 6ce2934fa99388e1d03ccbb46210fec37bb88044
   for i := 1 to N do
     A[i] := random(1000) - 500;
   for i := 1 to N do
     Write(A[i], ' ');
   writeln();
 
-<<<<<<< HEAD
   writeln(MaxValue(A) - MinValue(A));
-=======
-  x := MaxValue(PInteger(@A[1]),High(A)+1);
-  y := MinValue(PInteger(@A[1]),High(A)+1);
-
-  writeln(x - y);
->>>>>>> 6ce2934fa99388e1d03ccbb46210fec37bb88044
   readln();
 end.
