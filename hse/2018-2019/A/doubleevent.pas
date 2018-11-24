8fb32af2872395f4eec22e908466bcfe35c5aa17
Mikhail <mr.dolgih.2000@mail.ru>
2018-11-24 11:59:31 +0300

    2018-2019

---
+++ hse/2018-2019/A/doubleevent.pas
@@ -0,0 +1,17 @@
+program doubleevent;
+
+uses math;
+
+var
+  N, P, Q, x, y, a, b:int64;
+
+begin
+  readln(N, P, Q);
+  x:=max(P, Q);
+  y:=min(P, Q);
+
+  a:= ifthen(y-(N-x)>=1, y-(N-x), 1);
+  b := ifthen(x+y-1>=N, N, x+y-1);
+  writeln(a,' ', b);
+end.
+
