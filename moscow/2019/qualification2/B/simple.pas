82d5e62ab26469e15a991d8122580768dbcf80d9
Mikhail <mr.dolgih.2000@mail.ru>
2019-03-02 00:29:22 +0300

    B

--- moscow/2019/qualification2/B/simple.pas
+++ moscow/2019/qualification2/B/simple.pas
@@ -8,91 +8,57 @@ uses
 var
   i: integer;
   s, a, b, c: shortstring;
-  f: boolean;
+  LetterFound, dotFound: boolean;
 
-  procedure extra(s: shortstring);
+  function min(l, r: shortstring): shortstring;
   begin
-    a := extractword(1, s, [#9]);
-    b := extractword(2, s, [#9]);
-    c := extractword(3, s, [#9]);
+    exit(ifthen(l < r, l, r));
   end;
 
-  function minint(s: shortstring): shortstring;
+  function minint(l, r: shortstring): shortstring;
   var
-    a1, b1, c1: int64;
-    x: shortstring;
+    x, y: int64;
   begin
-    a1 := strtoint64(extractword(1, s, [#9]));
-    b1 := strtoint64(extractword(2, s, [#9]));
-    c1 := strtoint64(extractword(3, s, [#9]));
-    if (a1 < b1) and (a1 < c1) then
-      x := a
-    else
-    if b1 < c1 then
-      x := b
-    else
-      x := c;
-    exit(x);
+    x := strtoint64(l);
+    y := strtoint64(r);
+    exit(ifthen(x < y, l, r));
   end;
 
-  function minfloat(s: shortstring): shortstring;
+  function minfloat(l, r: shortstring): shortstring;
   var
-    i, n: byte;
-    a1, b1, c1: float;
-    x: shortstring;
+    x, y: float;
+    fs: TFormatSettings;
   begin
-    n := length(s);
-    for i := 1 to n do
-      if s[i] = '.' then
-        s[i] := ',';
-    a1 := strtofloat(extractword(1, s, [#9]));
-    b1 := strtofloat(extractword(2, s, [#9]));
-    c1 := strtofloat(extractword(3, s, [#9]));
-    if (a1 < b1) and (a1 < c1) then
-      x := a
-    else
-    if b1 < c1 then
-      x := b
-    else
-      x := c;
-    exit(x);
-  end;
-
-  function min(s: shortstring): shortstring;
-  var
-    x: shortstring;
-  begin
-    if (a < b) and (a < c) then
-      x := a
-    else
-    if b < c then
-      x := b
-    else
-      x := c;
-    exit(x);
+    fs := DefaultFormatSettings;
+    fs.DecimalSeparator := '.';
+    x := strTofloat(l, fs);
+    y := strTofloat(r, fs);
+    exit(ifthen(x < y, l, r));
   end;
 
 begin
- // Assign(input, 'tests/01');
-  // Assign(output, 'tests/00.a');
+  // Assign(input, 'tests/01');
   //reset(input);
-  //rewrite(output);
 
   while not EOF(input) do
   begin
     readln(s);
-    f := False;
+    a := extractword(1, s, [#9]);
+    b := extractword(2, s, [#9]);
+    c := extractword(3, s, [#9]);
+
+    LetterFound := False;
     for i := 1 to length(s) do
       if s[i] > #64 then
-        f := True;
-    extra(s);
-    if pos('.', s) > 0 then
-      writeln(minfloat(s))
+        LetterFound := True;
+    dotFound := pos('.', s) > 0;
+
+    if dotFound then
+      writeln(minfloat(minfloat(a, b), c))
     else
-    if f = True then
-      writeln(min(s))
+    if LetterFound then
+      writeln(min(min(a, b), c))
     else
-      writeln(minint(s));
+      writeln(minint(minint(a, b), c));
   end;
-  Close(input);
 end.
