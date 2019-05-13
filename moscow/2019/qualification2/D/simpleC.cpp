1eef9fb54e4502c1437fcc84198682feb2c5a281
mishadolgih <37023183+mishadolgih@users.noreply.github.com>
2019-05-13 17:23:23 +0300

    home

--- moscow/2019/qualification2/D/simpleC.cpp
+++ moscow/2019/qualification2/D/simpleC.cpp
@@ -6,19 +6,19 @@ int N, K;
 
 int main()
 {
-   freopen("tests/00", "r", stdin);
+    freopen("tests/00", "r", stdin);
     cin >> N >> K;
     int *a = new int[N];
     for (int i = 0; i < N; i++)
         cin >> a[i];
 
     int t[K];
-    for (int i = 0; i < (N-K+2); i++){
-        for (int j = i; i < (i+K); i++)
-            t[j] = a[j];
-        sort(t, t + K - 1);
-        int m = (i + (i + K - 1)) / 2;
-        cout << t[m];
+    int m = (K - 1) / 2;
+    for (int i = 0; i < (N-K+1); i++){
+        for (int j = 0; j < K; j++)
+            t[j] = a[i+j];
+        sort(t, t + K);
+        cout << t[m] << ' ';
     }
 
     return 0;
