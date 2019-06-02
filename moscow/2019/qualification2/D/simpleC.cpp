782623b2dbb18bd04cd4d930219ac1b81d8d5d99
mishadolgih <37023183+mishadolgih@users.noreply.github.com>
2019-06-02 18:39:07 +0300

    home

--- moscow/2019/qualification2/D/simpleC.cpp
+++ moscow/2019/qualification2/D/simpleC.cpp
@@ -4,25 +4,31 @@
 
 
 using namespace std;
-long long N, K;
+int N, K;
 
 int main()
 {
-//   freopen("tests/00", "r", stdin);
+   freopen("tests/02", "r", stdin);
     cin >> N >> K;
     long long *a = new long long[N];
-    for (long long i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
         cin >> a[i];
 
-    long long sum = 0;
-    for (long long i = 0; i < K; i++)
-        sum = sum + a[i];
-    cout << round((double)sum / K) << ' ';
-
-    for (long long i = 1; i < (N+1-K); i++){
-        sum = sum - a[i-1] + a[i + K - 1];
-        cout << round((double)sum / K) << ' ';
-    }
+//    sort(a, a+K);
+//    for (int i = 1; i < (N + 1 + K); i ++)
+//
+//    for (long long i = 0; i < K; i++)
+//        sum = sum + a[i];
+//    cout << round((double)sum / K) << ' ';
+//
+//    for (long long i = 1; i < (N+1-K); i++){
+//        sum = sum - a[i-1] + a[i + K - 1];
+//        cout << round((double)sum / K) << ' ';
+//    }
+    long long m = a[0];
+    for (int i = 1; i < N; i++)
+        m = max(m, a[i]);
+    cout << m;
 
     return 0;
 }
