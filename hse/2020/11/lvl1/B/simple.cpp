7a42d8621dfde8d6a6efdc1481a532f1502bc760
mishadolgih <37023183+mishadolgih@users.noreply.github.com>
2019-12-19 17:26:37 +0300

    home

--- hse/2020/11/lvl1/B/simple.cpp
+++ hse/2020/11/lvl1/B/simple.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <set>
-#include <utility>
+#include <map>
 #define ll long long
 #define X first
 #define Y second
@@ -8,7 +8,14 @@
 using namespace std;
 int n, k, *a, *b;
 
-ll f(int i, int j, int h);
+struct trio {int i, j, h;};
+inline int idx(trio t) {return t.i * k * 4 + t.j * 4 + t.h; }
+bool operator<(const trio &l, const trio &r) {return idx(l) < idx(r);}
+map<trio, ll> cache;
+
+pair<ll, int> g(int i, int j, int h);
+ll f(int i, int j, int h) {trio t = {i,j,h}; return j > 0
+    ? (cache.count(t)>0?cache[t]:cache[t]=g(i, j, h).X) : 0;}
 pair<ll, int> g(int i, int j, int h){
     set<pair<ll, int>> s;
     if (n - i > j)
@@ -21,11 +28,10 @@ pair<ll, int> g(int i, int j, int h){
         s.insert({f(i + 1, j - 1, 3)   + a[i] + b[i], 3});
     return *(--s.end());
 }
-ll f(int i, int j, int h) {return j > 0 ? g(i, j, h).X : 0;}
 
 int main()
 {
-//    freopen("tests/03", "r", stdin);
+    freopen("tests/03", "r", stdin);
     cin >> n >> k;
     a = new int[n], b = new int[n];
     for (int i = 0; i < n; i++)
