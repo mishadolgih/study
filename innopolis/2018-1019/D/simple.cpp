de2990ceddff64e594f8ea0796e0dfe5603f1177
mishadolgih <37023183+mishadolgih@users.noreply.github.com>
2019-12-19 17:54:02 +0300

    hone

--- innopolis/2018-1019/D/simple.cpp
+++ innopolis/2018-1019/D/simple.cpp
@@ -2,29 +2,33 @@
 #define ll long long
 
 using namespace std;
+ll m = 1000000000000000000;
 int n, s, v;
 int *a;
 
 ll f(int i, int j, int h){
-    if (h >= 0 && i <= n)    {
-        for (int k = 0; k <= v; j++)
-            return
-
-    }
-    else
+    if (j == 0){
+        cout << i << ' ' << j << ' ' << h << endl;
         return 0;
+    }
+
+    else
+    for (int k = max(j -(n - i -1)*v, 0); k <= min(j, v); k++){
+        cout << i << ' ' << j << ' ' << h << endl;
+        return (a[i]*k*h + f(i +1, j-k, k));
+    }
 
 }
 
 int main()
 {
-    freopen("tests/00", "r", stdin);
+    freopen("tests/02", "r", stdin);
     cin >> n >> s >> v;
-    a= new int[n];
-    for(int i = 0; i < (n- 1); i++)
+    a= new int[n+1];
+    for(int i = 1; i < n; i++)
         cin >> a[i];
 
-    cout << f(0, 0, s);
+    cout << f(0, s, 0);
 
     return 0;
 }
